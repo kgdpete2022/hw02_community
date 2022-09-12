1547c04afe5d934d7ba8ca220c5c54152a5fe592
Пётр Шопин <mail4dev2022@gmail.com>
2022-09-12 15:17:26 +0200

    7 tests passed, all errors corrected - 2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -8,9 +8,10 @@ class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
