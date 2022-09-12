958824e12381cfe3fdf0c32bfc9d1147279e25cb
Пётр Шопин <mail4dev2022@gmail.com>
2022-09-12 20:48:04 +0200

    Внесены исправления по комментариям ревью №1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,26 +5,28 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField()
+    title = models.CharField("Название группы", max_length=200)
+    slug = models.SlugField("URL-адрес группы", max_length=200, unique=True)
+    description = models.TextField("Описание группы")
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField("Текст публикации")
+    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts',
+        verbose_name="Автор публикации",
     )
-    group = models.ForeignKey(
+    group = models.ForeignKey(     
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name='posts',
         blank=True,
         null=True,
+        verbose_name="Группа публикации"
     )
